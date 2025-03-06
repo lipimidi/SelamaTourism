7b5f6b46aa161352ef1ad3f11d42cef1aad928c7
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2025-03-07 01:18:27 +0800

    test
    2

--- includes/settings.php
+++ includes/settings.php
@@ -5,7 +5,7 @@ $is_local = ($_SERVER['HTTP_HOST'] === 'localhost');
 
 // Set the correct base path
 $basePath = $is_local ? 'SelamaTourism' : ''; // Local subfolder, empty for production
-$basePath2 = $is_local ? '/SelamaTourism' : '/'; // Local subfolder with slash, empty for production
+$basePath2 = $is_local ? '/SelamaTourism' : ''; // Local subfolder with slash, empty for production
 
 $rootPath = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/') . '/';
 date_default_timezone_set('Asia/Kuala_Lumpur');
