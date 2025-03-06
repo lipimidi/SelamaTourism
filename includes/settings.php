d7aaa22a1615ef7d51749d0b10213c47801d35f5
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2025-03-07 01:27:37 +0800

    bug

--- includes/settings.php
+++ includes/settings.php
@@ -7,5 +7,5 @@ $is_local = ($_SERVER['HTTP_HOST'] === 'localhost');
 $basePath = $is_local ? 'SelamaTourism' : ''; // Local subfolder, empty for production
 $basePath2 = $is_local ? '/SelamaTourism' : ''; // Local subfolder with slash, empty for production
 
-$rootPath = $is_local ? '/SelamaTourism' : $_SERVER['HTTP_HOST'] ;
+$rootPath = $is_local ? '/SelamaTourism' : '' ;
 date_default_timezone_set('Asia/Kuala_Lumpur');
