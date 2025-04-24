5e831b35668c5d73d4eaaacc772aa329528a8f9a
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2025-04-24 09:32:12 +0800

    fdafds
    sdfdsfds

--- selama.sql
+++ selama.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 20, 2025 at 10:06 PM
+-- Generation Time: Apr 24, 2025 at 01:30 AM
 -- Server version: 8.3.0
 -- PHP Version: 8.2.18
 
@@ -39,15 +39,16 @@ CREATE TABLE IF NOT EXISTS `bookings` (
   `reason` text COLLATE utf8mb4_general_ci,
   PRIMARY KEY (`id`),
   KEY `timeslot_id` (`timeslot_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=65 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `bookings`
 --
 
 INSERT INTO `bookings` (`id`, `user_id`, `booking_date`, `timeslot_id`, `people_booked`, `created_at`, `status`, `reason`) VALUES
-(62, 3, '2025-02-27', 1, 3, '2025-03-16 06:19:22', 0, 'invalid payment'),
-(64, 3, '2025-03-11', 1, 1, '2025-03-18 20:35:06', 0, 'invalid payment');
+(62, 3, '2025-02-27', 1, 3, '2025-03-16 06:19:22', 2, NULL),
+(64, 3, '2025-03-11', 1, 1, '2025-03-18 20:35:06', 0, 'invalid details'),
+(65, 8, '2025-03-12', 2, 2, '2025-03-22 14:45:00', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -65,19 +66,22 @@ CREATE TABLE IF NOT EXISTS `booking_details` (
   `address` text COLLATE utf8mb4_general_ci NOT NULL,
   `email` text COLLATE utf8mb4_general_ci NOT NULL,
   `file` text COLLATE utf8mb4_general_ci,
-  `status` int DEFAULT '0',
+  `status` int DEFAULT '2',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=76 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  PRIMARY KEY (`id`),
+  KEY `booking_id` (`booking_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `booking_details`
 --
 
 INSERT INTO `booking_details` (`id`, `booking_id`, `name`, `ic`, `phone`, `address`, `email`, `file`, `status`, `created_at`) VALUES
-(75, 64, 'asd', 'asd', 'asd', 'asd', 'asdasd@gmail.com', 'final_cleaned_logo.png', 0, '2025-03-18 20:35:06'),
-(74, 62, 'asd2', 'asdd', 'asdd', 'asd', 'asdasd@gmail.com', 'cleaned_logo.png', 0, '2025-03-16 06:19:22'),
-(73, 62, 'asd1', 'asd', 'asd', 'asd', 'asdasd@gmail.com', 'final_cleaned_logo.png', 0, '2025-03-16 06:19:22');
+(73, 62, 'asd1', 'asd', 'asd', 'asd', 'asdasd@gmail.com', 'final_cleaned_logo.png', 1, '2025-03-16 06:19:22'),
+(74, 62, 'asd2', 'asdd', 'asdd', 'asd', 'asdasd@gmail.com', 'cleaned_logo.png', 2, '2025-03-16 06:19:22'),
+(75, 64, 'asd', 'asd', 'asd', 'asd', 'asdasd@gmail.com', 'final_cleaned_logo.png', 1, '2025-03-18 20:35:06'),
+(76, 65, 'test123', '123', '123', '123123', 'test@123', 'final_cleaned_logo.png', 1, '2025-03-22 14:45:00'),
+(77, 65, 'asdasdsa', 'asdasdasd', '12312321', 'asdasdsadas', 'asdasd', 'resume 2024 comp final final (2).pdf', 1, '2025-03-22 14:45:00');
 
 -- --------------------------------------------------------
 
@@ -91,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `booking_timeslots` (
   `start_time` time NOT NULL,
   `end_time` time NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `booking_timeslots`
@@ -100,6 +104,95 @@ CREATE TABLE IF NOT EXISTS `booking_timeslots` (
 INSERT INTO `booking_timeslots` (`id`, `start_time`, `end_time`) VALUES
 (1, '08:00:00', '12:00:00'),
 (2, '14:00:00', '18:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guides`
+--
+
+DROP TABLE IF EXISTS `guides`;
+CREATE TABLE IF NOT EXISTS `guides` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` date DEFAULT NULL,
+  `timeslot_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `guides`
+--
+
+INSERT INTO `guides` (`id`, `date`, `timeslot_id`, `user_id`, `created_at`, `status`) VALUES
+(10, '2025-02-27', 1, 5, '2025-03-22 20:33:02', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guide_details`
+--
+
+DROP TABLE IF EXISTS `guide_details`;
+CREATE TABLE IF NOT EXISTS `guide_details` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `booking_id` int NOT NULL,
+  `name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `ic` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `phone` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `address` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `file` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `status` int DEFAULT '2',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `booking_id` (`booking_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `guide_details`
+--
+
+INSERT INTO `guide_details` (`id`, `booking_id`, `name`, `ic`, `phone`, `address`, `email`, `file`, `status`, `created_at`) VALUES
+(73, 62, 'asd1', 'asd', 'asd', 'asd', 'asdasd@gmail.com', 'final_cleaned_logo.png', 1, '2025-03-16 06:19:22'),
+(74, 62, 'asd2', 'asdd', 'asdd', 'asd', 'asdasd@gmail.com', 'cleaned_logo.png', 2, '2025-03-16 06:19:22'),
+(75, 64, 'asd', 'asd', 'asd', 'asd', 'asdasd@gmail.com', 'final_cleaned_logo.png', 1, '2025-03-18 20:35:06'),
+(76, 65, 'test123', '123', '123', '123123', 'test@123', 'final_cleaned_logo.png', 1, '2025-03-22 14:45:00'),
+(77, 65, 'asdasdsa', 'asdasdasd', '12312321', 'asdasdsadas', 'asdasd', 'resume 2024 comp final final (2).pdf', 1, '2025-03-22 14:45:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log`
+--
+
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE IF NOT EXISTS `log` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `action` text COLLATE utf8mb4_general_ci,
+  `user_id` int DEFAULT NULL,
+  `description` text COLLATE utf8mb4_general_ci,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+DROP TABLE IF EXISTS `password_resets`;
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `expires_at` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -116,18 +209,20 @@ CREATE TABLE IF NOT EXISTS `users` (
   `role` int NOT NULL,
   `image` text COLLATE utf8mb4_general_ci,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  PRIMARY KEY (`id`),
+  KEY `role` (`role`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`, `image`, `created_at`) VALUES
-(4, 'admin', 'admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 1, NULL, '2025-03-18 21:26:54'),
 (3, 'asdasd', 'asdasd@gmail.com', 'a8f5f167f44f4964e6c998dee827110c', 3, NULL, '2025-03-14 23:20:06'),
-(5, 'guide', 'guide@gmail.com', '5a764d2ed111e518b94592517efbc3cf', 2, NULL, '2025-03-20 19:03:38'),
-(7, 'guide2', 'guide2@gmail.com', 'e7e8de62a6470356b5126d28ff4a730a', 2, NULL, '2025-03-20 21:11:38');
+(4, 'admin', 'admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 1, NULL, '2025-03-18 21:26:54'),
+(5, 'guide', 'guide@gmail.com', 'a8f5f167f44f4964e6c998dee827110c', 2, NULL, '2025-03-20 19:03:38'),
+(7, 'guide2', 'guide2@gmail.com', 'e7e8de62a6470356b5126d28ff4a730a', 2, NULL, '2025-03-20 21:11:38'),
+(9, 'dsaads', 'syakir@gmail.com', '5c023eb596d29068afa7400428547fcd', 1, NULL, '2025-04-12 10:53:38');
 
 -- --------------------------------------------------------
 
@@ -145,17 +240,18 @@ CREATE TABLE IF NOT EXISTS `user_details` (
   `passport` text COLLATE utf8mb4_general_ci,
   `phone` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `address` text COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user_details`
 --
 
 INSERT INTO `user_details` (`id`, `user_id`, `name`, `ic`, `image`, `passport`, `phone`, `address`) VALUES
-(1, 1, 'test', 'test', NULL, NULL, 'test', 'test'),
-(2, 5, '', '123123', NULL, NULL, '123123', 'guide'),
-(4, 7, 'guide2', '123123', NULL, NULL, '123123', '123123');
+(2, 5, 'abe', '123123', NULL, NULL, '123123', 'guide'),
+(4, 7, 'guide2', '123123', NULL, NULL, '123123', '123123'),
+(5, 9, 'asd', 'asdsadasd', NULL, NULL, 'asdasd', 'asdasdasdasdas');
 
 -- --------------------------------------------------------
 
@@ -168,7 +264,7 @@ CREATE TABLE IF NOT EXISTS `user_role` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user_role`
@@ -178,6 +274,40 @@ INSERT INTO `user_role` (`id`, `name`) VALUES
 (1, 'admin'),
 (2, 'guide'),
 (3, 'user');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`timeslot_id`) REFERENCES `booking_timeslots` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `booking_details`
+--
+ALTER TABLE `booking_details`
+  ADD CONSTRAINT `booking_details_ibfk_1` FOREIGN KEY (`booking_id`) REFERENCES `bookings` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `guide_details`
+--
+ALTER TABLE `guide_details`
+  ADD CONSTRAINT `guide_details_ibfk_1` FOREIGN KEY (`booking_id`) REFERENCES `bookings` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role`) REFERENCES `user_role` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `user_details`
+--
+ALTER TABLE `user_details`
+  ADD CONSTRAINT `user_details_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
