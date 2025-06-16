18c53b3adaf50b1e916eba52a7bdb01fa7baa0b8
lipimidi <halifi398@gmail.com>
2025-06-17 01:59:49 +0800

    Update sidenav.php

--- views/system/template/sidenav.php
+++ views/system/template/sidenav.php
@@ -1,6 +1,6 @@
 <div class="min-height-300 bg-gradient-primary position-absolute w-100"></div>
 
-<aside class="sidenav bg-koko navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4  shadow-lg"
+<aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4  shadow-lg"
 <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4  shadow-lg"
   id="sidenav-main">
   <div class="sidenav-header">
